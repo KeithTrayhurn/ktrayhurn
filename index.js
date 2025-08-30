f85261327961aaa477d2cb7bedcc5fa9b05363eb
KeithTrayhurn <keithtrayhurn@gmail.com>
2025-08-30 12:35:09 +1000

    Added SUMO25 draft and FRC2025, fixed tile cropping

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     if (event.target.classList.contains('fullscreenable')) {
       if (document.fullscreenElement) {
         document.exitFullscreen();
+        event.target.style.objectFit = 'cover'; // Reset to cover on exit
       } else {
         event.target.requestFullscreen();
+        event.target.style.objectFit = 'contain'; // Change to contain on enter
       }
     }
   });
